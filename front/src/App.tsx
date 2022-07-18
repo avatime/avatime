182f8ed5da59ff874700dec73bd8754b8318b85a
DESKTOP-KVCQHCD\SSAFY <>
2022-07-18 13:42:01 +0900

    feat(front): api, redux 참고 코드 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router";
+import { TestPage } from "./pages/TestPage";
+import { MainPage } from "./pages/MainPage";
+import store from "./stores";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Provider store={store}>
+        <PersistGate persistor={persistStore(store)}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<TestPage />} />
+              <Route path="/main" element={<MainPage />} />
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
     </div>
   );
 }
---
+++ front/src/apis/request/userReq.ts
@@ -0,0 +1,3 @@
+export interface UserLoginPostReq {
+    id: string;
+}
