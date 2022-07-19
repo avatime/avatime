af8ae61dabd31f44fd42a879087140f133965479
이주영 <dlendjs93@gmail.com>
2022-07-19 23:43:28 +0900

    feat(Chat): 채팅방 구현 #2

--- front/src/pages/SessionPage.tsx
+++ front/src/pages/SessionPage.tsx
@@ -1,13 +1,11 @@
-import React, { FC, Suspense } from "react";
+import React, { FC, Suspense, useState } from "react";
 import { useQuery } from "react-query";
 import { Conference } from "../components/session/Conference";
 import sessionApi from "../apis/sessionApi";
 import { SessionUserListRes } from "../apis/response/sessionRes";
 import { ChatRoom } from "../components/chat/ChatRoom";
-import { Grid } from "@mui/material";
-import chatApi from "../apis/chatApi";
+import { Box, Grid } from "@mui/material";
 import { ErrorBoundary } from "react-error-boundary";
-import { ChatRes } from "../apis/response/chatRes";
 
 interface IProps {}
 
@@ -18,13 +16,11 @@ export const SessionPage: FC<IProps> = (props) => {
     { suspense: true }
   );
 
-  const { data: chatData } = useQuery<any, Array<ChatRes>>(
-    "session/allChat",
-    () => chatApi.receive(),
-    {
-      suspense: true,
-    }
-  );
+  const [opened, setOpened] = useState<boolean[]>([true, true]);
+  const cntOpened = opened.filter((it) => it).length;
+
+  const fullHeight = "77vh";
+  const halfHeight = "32vh";
 
   return (
     <Grid container spacing={3} sx={{ float: "left" }}>
@@ -36,11 +32,24 @@ export const SessionPage: FC<IProps> = (props) => {
         </Suspense>
       </Grid>
       <Grid item xs={3}>
-        <Suspense fallback={<h1>로딩중</h1>}>
-          <ErrorBoundary fallback={<h1>에러에러</h1>}>
-            <ChatRoom openable={true} chatType="all" chatList={chatData} size="half" />
-          </ErrorBoundary>
-        </Suspense>
+        <Box height="100vh" display="flex" flexDirection="column" sx={{ overflow: "hidden" }}>
+          <ChatRoom
+            chatType="all"
+            isOpened={opened[0]}
+            onClickHeader={() => {
+              setOpened((prev) => [!prev[0], prev[1]]);
+            }}
+            maxHeight={opened[0] && cntOpened === 1 ? fullHeight : halfHeight}
+          />
+          <ChatRoom
+            chatType="gender"
+            isOpened={opened[1]}
+            onClickHeader={() => {
+              setOpened((prev) => [prev[0], !prev[1]]);
+            }}
+            maxHeight={opened[1] && cntOpened === 1 ? fullHeight : halfHeight}
+          />
+        </Box>
       </Grid>
     </Grid>
   );
