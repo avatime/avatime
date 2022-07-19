98b61099fa4cee54a41fdc1ceedc9c70cb63c0e2
이주영 <dlendjs93@gmail.com>
2022-07-20 01:05:34 +0900

    feat(Chat): 채팅방 구현 #3
    - 시간 AA h:mm 형식으로 포맷팅
    - 남의 메시지 / 내 메시지 구분
    - 사용자 name / 시간 표시 카톡처럼

--- front/src/components/chat/ChatBlock.tsx
+++ front/src/components/chat/ChatBlock.tsx
@@ -1,22 +1,38 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import React, { FC } from "react";
 import { ChatRes } from "../../apis/response/chatRes";
+import { formatDate } from "../../utils/day";
+
+type Order = "left" | "right";
 
 interface IProps {
   chatRes: ChatRes;
+  order: Order;
+  showTime: boolean;
+  showName: boolean;
 }
 
-export const ChatBlock: FC<IProps> = ({ chatRes }) => {
+export const ChatBlock: FC<IProps> = ({ chatRes, order, showTime, showName }) => {
   return (
-    <Stack mb={2}>
-      <Stack direction="row" alignItems="flex-end" spacing={2}>
-        <Typography variant="subtitle1">{chatRes.name}</Typography>
-        <Typography variant="subtitle2">{chatRes.time}</Typography>
-      </Stack>
-      <Stack alignItems="flex-start">
-        <Typography className="chat__bubble" variant="subtitle2">
+    <Stack alignItems="stretch" pl={2} pr={2} p={0.5} pb={showTime ? 1 : 0} ml={1} mr={1}>
+      {order === "left" && showName ? (
+        <Stack direction="row">
+          <Typography variant="subtitle1">{chatRes.name}</Typography>
+        </Stack>
+      ) : null}
+
+      <Stack direction={order === "left" ? "row" : "row-reverse"} alignItems="flex-end">
+        <Typography
+          className={`chat__bubble__${order} chat__bubble${showName && "__arrow"}__${order}`}
+          variant="subtitle2"
+        >
           {chatRes.message}
         </Typography>
+        {showTime ? (
+          <Typography variant="subtitle2" sx={{ whiteSpace: "nowrap" }} ml={1} mr={1}>
+            {formatDate(chatRes.time, "A h:mm")}
+          </Typography>
+        ) : null}
       </Stack>
     </Stack>
   );
