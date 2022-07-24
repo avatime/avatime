7c14cb22b0427355a4c9b183034cdc87e8b7e426
이주영 <dlendjs93@gmail.com>
2022-07-24 21:37:53 +0900

    refactor(session): openvidu 관련 객체 redux에서 제거. props로 전달.
    - redux로 넣으면 openvidu 객체를 JSON화 시켜야하는데, 여기서 무한 루프에 빠짐.

--- front/src/stores/slices/meetingSlice.ts
+++ front/src/stores/slices/meetingSlice.ts
@@ -5,39 +5,8 @@ const meetingSlice = createSlice({
   initialState: {
     roomId: 0,
     headCount: 8,
-    // 아래는 openvidu와 관련
-    publisher: undefined,
-    subscribers: [] as any[],
-    currentVideoDevice: undefined,
   },
-  reducers: {
-    setPublisher(state, action) {
-      state.publisher = action.payload;
-    },
-    pushSubscribers(state, action) {
-      state.subscribers.push(action.payload);
-    },
-    removeSubscribers(state, action) {
-      let index = state.subscribers.indexOf(action.payload, 0);
-      if (index > -1) {
-        state.subscribers.splice(index, 1);
-      }
-    },
-    clearSubscribers(state) {
-      state.subscribers = [];
-    },
-    setCurrentVideoDevice(state, action) {
-      state.currentVideoDevice = action.payload;
-    },
-  },
+  reducers: {},
 });
-
-export const {
-  setPublisher,
-  pushSubscribers,
-  removeSubscribers,
-  clearSubscribers,
-  setCurrentVideoDevice,
-} = meetingSlice.actions;
 
 export default meetingSlice.reducer;
