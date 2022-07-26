e6ea3449e7b02b079d7ab3181e083063f3a7ff3f
채윤선 <yoonsun0809@gmail.com>
2022-07-26 14:14:02 +0900

    feat(mainheaer) : 프로필 박스, 하이퍼링크 추가, CSS 수정
    추후 프로필 클릭시 나오는 못생긴 박스 없애기

--- front/src/components/main/MainHeader.tsx
+++ front/src/components/main/MainHeader.tsx
@@ -1,15 +1,68 @@
-import React, { FC } from 'react'
-import Avatar from '@mui/material/Avatar';
+import React, { FC } from "react";
+import Avatar from "@mui/material/Avatar";
+import logo from "../../assets/avartimeLogo.png";
+import { Box, Menu, MenuItem, MenuList} from "@mui/material";
+import { Link } from "react-router-dom";
+import Tooltip from "@mui/material/Tooltip";
+import "../../style.css";
+import { padding } from "@mui/system";
 interface IProps {}
 
 /**
-* @author
-* @function @MainHeader
-**/
+ * @author
+ * @function @MainHeader
+ **/
 
-export const MainHeader:FC<IProps> = (props) => {
+export const MainHeader: FC<IProps> = (props) => {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
-    <div style={{position:"absolute",top:10, right:"5%"}}>  <Avatar alt="example" src="../../assets/profileEx.jpg"  sx={{ width: 56, height: 56 }} />
-    </div>
-   )
- }
+    <>
+      <Box display="flex" justifyContent="right" alignItems="center" marginBottom="2%">
+        <Link to="/main">
+          <img src={logo} alt="로고" style={{ width: "70%", paddingTop: "2%" }} />
+        </Link>
+
+        <Tooltip title="설정">
+          <Avatar
+            alt="example"
+            src="../../assets/profileEx.jpg"
+            sx={{ width: 56, height: 56 }}
+            style={{marginLeft:"27%", marginRight:"3%"}}
+            onClick={handleOpenUserMenu}
+          />
+        </Tooltip>
+        <Menu
+          sx={{ mt: "60px"}}
+          id="profilemenu"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuList onClick={handleCloseUserMenu} sx={{textAlign:"center" , border:"none"}}>
+            <MenuItem>
+              <Link to="/mypage" style={{textDecoration: "none"}}>마이페이지</Link>
+            </MenuItem>
+            <MenuItem>로그아웃</MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
+    </>
+  );
+};
