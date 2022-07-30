1a3ad9e8d7af663274117786c35c6965af23496a
DESKTOP-KVCQHCD\SSAFY <dlwlsk0428@naver.com>
2022-07-30 13:05:14 +0900

    feat(chatting): 요청시 채팅 전문 발송

--- back/src/main/java/com/ssafy/api/controller/MessageController.java
+++ back/src/main/java/com/ssafy/api/controller/MessageController.java
@@ -17,7 +17,6 @@ import com.ssafy.api.service.ChatService;
 import com.ssafy.api.service.ChattingMessageService;
 import com.ssafy.db.entity.ChattingMessage;
 import com.ssafy.db.repository.ChattingMessageRepository;
-import com.ssafy.db.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -31,16 +30,22 @@ public class MessageController {
     private final ChatService chattingRoomService;
     private final ChattingMessageService chattingMessageService;
     private final ChattingMessageRepository chattingMessageRepository;
-    private final UserRepository userRepository;
     
     @PostMapping("/send")
     public void sendMessage(@RequestBody ChattingMessagePostReq messageReq) throws Exception {
+    	if (ChattingMessagePostReq.MessageType.ENTER.equals(messageReq.getType())) {
+    		messageReq.setMessage(chattingMessageService.findUserName(chattingRoomService.findByChatId(messageReq.getChattingroom_id()), messageReq.getUser_id())+"님이 입장하였습니다.");
+        } 
+    	else if (ChattingMessagePostReq.MessageType.LEAVE.equals(messageReq.getType())) {
+    		messageReq.setMessage(chattingMessageService.findUserName(chattingRoomService.findByChatId(messageReq.getChattingroom_id()), messageReq.getUser_id())+"님이 퇴장하였습니다.");
+        }
     	ChattingMessage message = ChattingMessage.builder()
     			.userId(messageReq.getUser_id())
     			.content(messageReq.getMessage())
     			.chattingRoom(chattingRoomService.findByChatId(messageReq.getChattingroom_id()))
     			.build();
     	message = chattingMessageRepository.save(message);
+    	;
     	List<ChattingMessage> messages = chattingMessageService.findByChattingRoomId(message.getChattingRoom().getId());
     	List<ChattingMessageRes> list = new ArrayList<>();
     	for(ChattingMessage m : messages) {
