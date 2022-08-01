5ca12f54da98f4941090651a25c01f0c78cf01ae
민경욱 <rrkcl7733@naver.com>
2022-08-01 21:25:14 +0900

    feat(gender): 대기방 성별 쿼리 작성

--- back/src/main/java/com/ssafy/config/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -15,7 +15,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/ava").setAllowedOriginPatterns("*").withSockJS();
+//        registry.addEndpoint("/ws/ava").setAllowedOriginPatterns("*").withSockJS();
+    	registry.addEndpoint("/ws/ava");
     }
 
     @Override
