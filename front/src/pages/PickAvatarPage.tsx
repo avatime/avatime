979c1420e10bd8e97dc65d6e6e8c51f04aad31a6
채윤선 <('')>
2022-08-03 15:17:23 +0900

    feat(pickAvatar) : 아바타 api 추가

--- front/src/pages/PickAvatarPage.tsx
+++ front/src/pages/PickAvatarPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import { MainHeader } from "../components/main/MainHeader";
 import { Box, display } from "@mui/system";
@@ -6,12 +6,45 @@ import { Box, display } from "@mui/system";
 import grey from "@mui/material/colors/grey";
 
 import { GaugeBar } from "../components/pickAvatar/GaugeBar";
+import { AvatarPickInfoRes } from "../apis/response/avatarRes";
+
+import SockJS from "sockjs-client";
+import * as Stomp from "stompjs";
+import { SessionModal } from "../components/session/modal/SessionModal";
+import { Typography } from "@mui/material";
+
 
 interface IProps {};
 
 
 export const PickAvatarPage: FC<IProps> = () => {
-  
+  //소켓 통신-----------------------------------------------------------------
+  const [originData, setOriginData] = useState<AvatarPickInfoRes[]>([]);
+  const [stompClient, setStompClient] = useState<any>();
+  useEffect(() => {
+    if (stompClient) {
+      return;
+    }
+
+    const socket = new SockJS("http://localhost:8080/ws/ava");
+    const client = Stomp.over(socket);
+    client.connect({}, function (frame) {
+      console.log("소켓 연결 성공", frame);
+
+      client.subscribe("/topic/getList", function (response) {
+        console.log(response.body);
+        setOriginData(JSON.parse(response.body));
+      });
+      client.send("/app/getList", {}, "aaa");
+    });
+
+    setStompClient(client);
+  }, [stompClient]);
+
+
+
+
+  //----------------------------------------------------------------------------------
 
   return (
     <div className="mainback" style={{ display: "flex", flexDirection: "column" }}>
@@ -30,6 +63,7 @@ export const PickAvatarPage: FC<IProps> = () => {
         </Grid>
       </Box>
       <GaugeBar total={100} current={50}/>
+      
     
     </div>
   );
