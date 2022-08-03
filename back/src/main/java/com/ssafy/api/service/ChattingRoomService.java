d7fee5a13b9eff44bc5ce8c70d7862579c0249cd
김찬영 <>
2022-08-03 17:10:18 +0900

    refactor(properties): mysql 설정 관련

--- back/src/main/java/com/ssafy/api/service/ChattingRoomService.java
+++ back/src/main/java/com/ssafy/api/service/ChattingRoomService.java
@@ -6,9 +6,6 @@ import com.ssafy.db.entity.ChattingRoom;
 
 public interface ChattingRoomService {
 	public ChattingRoom saveByWaitingRoom(Long roomId);
-<<<<<<< HEAD
 	public ChattingRoom createRoomInMeetingRoom(Long roomId);
-=======
 	Optional<ChattingRoom> findByRoomIdAndType(Long roomId);
->>>>>>> 8c18386 (feat(waitingstate): 접수처 구현)
 }
