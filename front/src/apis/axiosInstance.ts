80beeaca737b3569856fc19730f9d254d6958779
채윤선 <('')>
2022-08-05 15:14:49 +0900

    style(mainpage) : 표 크기 수정

--- front/src/apis/axiosInstance.ts
+++ front/src/apis/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const WS_BASE_URL = "http://i7a309.p.ssafy.io/ws/ava";
-export const API_BASE_URL = "http://i7a309.p.ssafy.io/api/v1";
+export const WS_BASE_URL = "http://localhost:8080/ws/ava";
+export const API_BASE_URL = "http://localhost:8080/api/v1";
 
 export let axiosInstance = axios.create({
   baseURL: API_BASE_URL,
