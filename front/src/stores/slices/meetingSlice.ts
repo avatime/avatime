673cd3f5e7a412ecc497e22750978e36ee05f989
이주영 <()>
2022-08-05 22:58:45 +0900

    feat(waiting): 아바타 선택 화면으로 이동하는 기능 추가

--- front/src/stores/slices/meetingSlice.ts
+++ front/src/stores/slices/meetingSlice.ts
@@ -1,18 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface MeetingState {
+  roomId: number | undefined;
+  totalChatRoomId: number | undefined;
+  genderChatRoomId: number | undefined;
+  userList: undefined;
+}
+
+const initialState: MeetingState = {
+  roomId: undefined,
+  totalChatRoomId: undefined,
+  genderChatRoomId: undefined,
+  userList: undefined,
+};
+
 const meetingSlice = createSlice({
   name: "meeting",
-  initialState: {
-    roomId: 0,
-    userList: undefined,
-  },
+  initialState,
   reducers: {
+    setMeetingRoomId(state, action) {
+      state.roomId = action.payload;
+    },
     setUserList(state, action) {
       state.userList = action.payload;
     },
   },
 });
 
-export const { setUserList } = meetingSlice.actions;
+export const { setUserList, setMeetingRoomId } = meetingSlice.actions;
 
 export default meetingSlice.reducer;
