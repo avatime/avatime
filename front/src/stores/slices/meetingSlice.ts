3b1a675660cb779d99f490f8eefa5594d85b9f71
이주영 <()>
2022-08-08 03:58:18 +0900

    feat(meeting): 아바타 선택 화면에 실제 아바타 적용 및 미팅 화면에 해당 가면 씌우는 기능 추가

--- front/src/stores/slices/meetingSlice.ts
+++ front/src/stores/slices/meetingSlice.ts
@@ -4,27 +4,18 @@ const meetingSlice = createSlice({
   name: "meeting",
   initialState: {
     roomId: undefined,
-    userList: [] as any[],
+    userInfoList: [] as any[],
   },
   reducers: {
     setMeetingRoomId(state, action) {
       state.roomId = action.payload;
     },
-    addUserList(state, action) {
-      state.userList.push(action.payload);
-    },
-    removeUserList(state, action) {
-      let index = state.userList.findIndex((it) => it.user_id);
-      if (-1 < index) {
-        state.userList.splice(index, 1);
-      }
-    },
-    clearUserList(state) {
-      state.userList = [];
+    setUserInfoList(state, action) {
+      state.userInfoList = action.payload;
     },
   },
 });
 
-export const { setMeetingRoomId, addUserList, removeUserList, clearUserList } = meetingSlice.actions;
+export const { setMeetingRoomId, setUserInfoList } = meetingSlice.actions;
 
 export default meetingSlice.reducer;
