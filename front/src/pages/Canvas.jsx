368b6c094116cda1574367ccca44aec119e4f378
정건우 <()>
2022-08-07 23:18:56 +0900

    ﻿feature(canvas) : 캔버스 테스트

---
+++ front/src/pages/Canvas.jsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import { ReactSketchCanvas } from "react-sketch-canvas";
+
+const styles = {
+  border: "0.0625rem solid #9c9c9c",
+  borderRadius: "0.25rem",
+};
+
+export const Canvas = class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.canvas = React.createRef();
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactSketchCanvas
+          ref={this.canvas}
+          strokeWidth={5}
+          strokeColor="black"
+          width="500px"
+          height="500px"
+        />
+        <button
+          onClick={() => {
+            this.canvas.current
+              .exportImage("png")
+              .then((data) => {
+                console.log(data);
+              })
+              .catch((e) => {
+                console.log(e);
+              });
+          }}
+        >
+          Get Image
+        </button>
+      </div>
+    );
+  }
+};
