8eaaf736e5c09ceffb16a860b789604023d12938
이주영 <dlendjs93@gmail.com>
2022-08-08 09:52:30 +0900

    refactor: 실서버 url로 변경

--- front/src/apis/url.ts
+++ front/src/apis/url.ts
@@ -1,4 +1,4 @@
-const DEV = true;
+const DEV = false;
 
 const PORT_WEB = ":3000";
 const PORT_SERVER = ":8080";
@@ -14,4 +14,4 @@ export const REDIRECT_URI_NAVER: string =
 export const WS_BASE_URL = (DEV ? `${URL_LOCAL}${PORT_SERVER}` : URL_RELEASE) + "/ws/ava";
 export const API_BASE_URL = (DEV ? `${URL_LOCAL}${PORT_SERVER}` : URL_RELEASE) + "/api/v1";
 
-export const OPENVIDU_SERVER_URL = (DEV ? "https://localhost" : URL_RELEASE) + PORT_OPENVIDU;
+export const OPENVIDU_SERVER_URL = (DEV ? "https://localhost" : "https://i7a309.p.ssafy.io") + PORT_OPENVIDU;
