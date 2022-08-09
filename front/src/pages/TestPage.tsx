034eba0aa155ef409f6a60cec8895ed502d34125
이주영 <()>
2022-08-09 23:21:31 +0900

    fix(meeting): 가면 둥둥 현상 수정

--- front/src/pages/TestPage.tsx
+++ front/src/pages/TestPage.tsx
@@ -4,21 +4,28 @@ import { Link } from "react-router-dom";
 interface IProps {}
 
 export const TestPage: FC<IProps> = (props) => {
-
   useEffect(() => {
     // socketIo();
-  }, [])
-  
+  }, []);
+
   return (
     <div>
-      <Link to="/main">MainPage</Link><br/>
-      <Link to="/waiting">WaitingPage</Link><br/>
-      <Link to="/session">SessionPage</Link><br/>
-      <Link to="/login">LoginPage</Link><br/>
-      <Link to="/mypage">MyPage</Link><br/>
-      <Link to="/finalPickResult">finalPickResult</Link><br/>
-      <Link to="/pickAvatar">PickAvatarPage</Link><br/>
-      <Link to="/canvas">Canvas</Link><br/>
+      <Link to="/main">MainPage</Link>
+      <br />
+      <Link to="/waiting">WaitingPage</Link>
+      <br />
+      <Link to="/session">SessionPage</Link>
+      <br />
+      <Link to="/login">LoginPage</Link>
+      <br />
+      <Link to="/mypage">MyPage</Link>
+      <br />
+      <Link to="/finalPickResult">finalPickResult</Link>
+      <br />
+      <Link to="/pickAvatar">PickAvatarPage</Link>
+      <br />
+      <Link to="/canvas">Canvas</Link>
+      <br />
       <Link to="/subSession">SubSession</Link>
     </div>
   );
