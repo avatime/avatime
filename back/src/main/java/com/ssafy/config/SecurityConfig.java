0748d6bbf4bd0cbf0e39489ec11e5e3edb32b800
민경욱 <('')>
2022-08-09 15:36:52 +0900

    feat(security): security config 추가

--- back/src/main/java/com/ssafy/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -54,14 +54,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
-                .and()
-                .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
-                .authorizeRequests()
-                .antMatchers("/api/v1/users/me").authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
-    	        	    .anyRequest().permitAll()
-                .and().cors();
+        	.httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
+            .and()
+            .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+            .authorizeRequests()
+            .antMatchers("/api/v1/users/me").authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
+    	    .anyRequest().permitAll()
+            .and().cors();
+        http
+        	.headers()
+        	.xssProtection()
+        	.and()
+        	.contentSecurityPolicy("script-src 'self'");
     }
 }
