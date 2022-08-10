0d690eb68b800584335c232e8cf44b526eb4fcef
채윤선 <yoonsun0809@gmail.com>
2022-08-11 01:18:57 +0900

    style(landingPage) : 랜딩페이지 완성

--- front/src/pages/Landing/LandingPage.tsx
+++ front/src/pages/Landing/LandingPage.tsx
@@ -2,6 +2,9 @@ import { Box } from "@mui/material";
 import React, { FC } from "react";
 import "./LandingPage.css";
 import { Link } from "react-router-dom";
+import "animate.css";
+import "aos/dist/aos.css";
+
 interface IProps {}
 
 /**
@@ -11,7 +14,7 @@ interface IProps {}
 
 export const LandingPage: FC<IProps> = (props) => {
   return (
-   
+    <div>
       <Box className="landing">
         <Link to="/login">
           <h1 className="landing--h1">
@@ -23,8 +26,27 @@ export const LandingPage: FC<IProps> = (props) => {
             <span className="landing--h1--span">M</span>
             <span className="landing--h1--span">E</span>
           </h1>
+          <br />
+          <Box p={3} />
+          <h2 className="landing--h2">
+            <span className="landing--h2--span">내</span>
+            <span className="landing--h2--span">가</span>
+            <span className="landing--h2--span">  </span>
+            <span className="landing--h2--span">만</span>
+            <span className="landing--h2--span">든</span>
+            <span className="landing--h2--span">  </span>
+            <span className="landing--h2---span">가</span>
+            <span className="landing--h2---span">면</span>
+            <span className="landing--h2--span">으</span>
+            <span className="landing--h2--span">로</span>
+            <span className="landing--h2--span">  </span>
+            <span className="landing--h2---span">미</span>
+            <span className="landing--h2---span">팅</span>
+            <span className="landing--h2--span">하</span>
+            <span className="landing--h2--span">자</span>
+          </h2>
         </Link>
       </Box>
-   
+    </div>
   );
 };
