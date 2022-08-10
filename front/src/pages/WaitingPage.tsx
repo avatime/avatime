9bd846577aae4a6aa14c509f27f3017d4b5a4896
이주영 <()>
2022-08-10 09:54:04 +0900

    style: 미팅방 나가기 버튼 색상 변경 및 대기방 유저 배치 변경

--- front/src/pages/WaitingPage.tsx
+++ front/src/pages/WaitingPage.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useMemo, useState } from "react";
 import { ChatRoom } from "../components/chat/ChatRoom";
-import { Badge, BadgeProps, Box, Grid, IconButton, styled, Typography, Stack } from '@mui/material';
+import { Badge, BadgeProps, Box, Grid, IconButton, styled, Typography, Stack } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { MainHeader } from "../components/main/MainHeader";
 import "../components/chat/style.css";
@@ -35,6 +35,8 @@ const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
 export const WaitingPage: FC<IProps> = (props) => {
   const waitingState = useSelector((state: any) => state.waiting);
   const userId = useSelector((state: any) => state.user.userId);
+  const headCount = useSelector((state: any) => state.waiting.headCount);
+  const gender = useSelector((state: any) => state.user.userGender);
 
   const [waitingUserList, setWaitingUserList] = useState<WaitingUser[]>([]);
   const [candidateList, setCandidateList] = useState<WaitingUser[]>([]);
@@ -114,27 +116,63 @@ export const WaitingPage: FC<IProps> = (props) => {
 
   const chatRoomId = useSelector((state: any) => state.waiting.chatRoomId);
 
+  const sameGenderUserList = useMemo(
+    () => waitingUserList.filter((it) => it.gender === gender),
+    [gender, waitingUserList]
+  );
+
+  const diffGenderUserList = useMemo(
+    () => waitingUserList.filter((it) => it.gender !== gender),
+    [gender, waitingUserList]
+  );
+
   return (
     <div className="mainback" style={{ display: "flex", flexDirection: "column" }}>
-      <Grid container spacing={3} p={2} sx={{ flex: "1" }} justifyContent="space-evenly">
+      <Grid container spacing={3} p={2} sx={{ flex: "1" }}>
         <Grid item xs={9} sx={{ float: "left", display: "flex", flexDirection: "column" }}>
           <Box pl={1}>
             <Typography variant="h4">
               {waitingState.roomName} / {waitingState.sido} / {waitingState.age}
             </Typography>
           </Box>
-          <Box flex={1} borderRadius="10px" bgcolor={grey[200]} p={2}>
-            <Grid container spacing={2} height="100%">
-              {waitingUserList.map((it) => (
-                <Grid item xs={3} key={it.id} height="50%">
-                  <WaitingUserProfile
-                    waitingUser={it}
-                    onClickAvatar={onOpenInfo}
-                    me={userId === it.id}
-                  />
-                </Grid>
-              ))}
-            </Grid>
+          <Box
+            display="flex"
+            flex={1}
+            flexDirection="column"
+            alignItems="stretch"
+            borderRadius="10px"
+            bgcolor={grey[200]}
+            p={2}
+          >
+            {[0, 1].map((outerIdx) => (
+              <Grid
+                key={outerIdx}
+                container
+                justifyContent="space-evenly"
+                alignItems="center"
+                spacing={2}
+                flex={1}
+              >
+                {Array.from(Array(headCount / 2)).map((_, innerIdx) => {
+                  const list = outerIdx === 0 ? sameGenderUserList : diffGenderUserList;
+                  const it = innerIdx < list.length ? list[innerIdx] : null;
+                  return (
+                    <Grid
+                      key={innerIdx}
+                      item
+                      xs={3}
+                      height="80%"
+                    >
+                      <WaitingUserProfile
+                        waitingUser={it}
+                        onClickAvatar={onOpenInfo}
+                        me={userId === it?.id}
+                      />
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            ))}
           </Box>
         </Grid>
         <Grid item xs={3} sx={{ float: "left", display: "flex", flexDirection: "column" }}>
