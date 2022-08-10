d3b4bee6f1bc5bf351b68de18391d9383924d6d6
이주영 <()>
2022-08-10 10:29:03 +0900

    refactor: 접수처에 성별 표시 추가 및 세션에서 나갈 때, 메인으로 이동하도록 변경

--- front/src/components/waitingRoom/CandidateUser.tsx
+++ front/src/components/waitingRoom/CandidateUser.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Typography, Box, IconButton } from "@mui/material";
+import { Avatar, Typography, Box, IconButton, useTheme } from "@mui/material";
 import React, { FC } from "react";
 import { WaitingUser } from "../../apis/response/waitingRoomRes";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -11,14 +11,21 @@ interface IProps {
 }
 
 export const CandidateUser: FC<IProps> = ({ waitingUser, onClickAccept, onClickRefuse }) => {
+  const theme = useTheme();
   return (
     <Box display="flex" flexDirection="row" alignItems="center" width="100%">
-      <Avatar />
+      <Avatar
+        sx={{
+          border: `solid 3px ${
+            waitingUser.gender === "M" ? theme.palette.primary.light : theme.palette.error.light
+          }`,
+        }}
+      />
       <Box p={1} />
       <Typography variant="subtitle2" flex={1}>
         {waitingUser.name}
       </Typography>
-      <IconButton color="success" onClick={() => onClickAccept(waitingUser.id)} >
+      <IconButton color="success" onClick={() => onClickAccept(waitingUser.id)}>
         <CheckCircleIcon />
       </IconButton>
       <IconButton color="error" onClick={() => onClickRefuse(waitingUser.id)}>
