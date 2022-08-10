42950e0f97682b4aee56a269ce08a5cf6e1a7a61
이주영 <dlendjs93@gmail.com>
2022-08-10 09:54:04 +0900

    style: 미팅방 나가기 버튼 색상 변경 및 대기방 유저 배치 변경

--- front/src/components/waitingRoom/WaitingUserProfile.tsx
+++ front/src/components/waitingRoom/WaitingUserProfile.tsx
@@ -1,12 +1,12 @@
-import { Avatar, Box, Typography, IconButton, useTheme } from '@mui/material';
+import { Avatar, Box, Typography, IconButton, useTheme } from "@mui/material";
 import React, { FC } from "react";
 import { WaitingUser } from "../../apis/response/waitingRoomRes";
 import Crown from "../../assets/crown.png";
 
 interface IProps {
-  waitingUser: WaitingUser;
-  onClickAvatar: (userId: number) => void;
-  me: boolean;
+  waitingUser: WaitingUser | null;
+  onClickAvatar?: (userId: number) => void;
+  me?: boolean;
 }
 
 export const WaitingUserProfile: FC<IProps> = ({ waitingUser, onClickAvatar, me }) => {
@@ -21,59 +21,65 @@ export const WaitingUserProfile: FC<IProps> = ({ waitingUser, onClickAvatar, me 
       justifyContent="center"
       border={me ? "solid 2px black" : ""}
     >
-      <Box
-        bgcolor={waitingUser.gender === "M" ? theme.palette.primary.light : theme.palette.error.light}
-        left="20px"
-        right="20px"
-        top="20px"
-        bottom="20px"
-        position="absolute"
-        borderRadius="50%"
-      >
-        <IconButton
-          onClick={() => onClickAvatar(waitingUser.id)}
-          sx={{
-            position: "absolute",
-            left: "10px",
-            right: "10px",
-            top: "10px",
-            bottom: "10px",
-            width: "auto",
-            height: "auto",
-          }}
-        >
-          <Avatar
-            src={waitingUser.profile_img_path}
-            alt="profile"
-            sx={{
-              width: "100%",
-              height: "100%",
-            }}
-          />
-        </IconButton>
-        {waitingUser.type === 0 && (
-          <img
-            src={Crown}
-            alt="방장"
-            style={{
-              width: "24px",
-              height: "24px",
-              position: "absolute",
-              right: "0",
-            }}
-          />
-        )}
-      </Box>
-      <Box
-        position="absolute"
-        bottom="0"
-        marginBottom="10px"
-        bgcolor="#4d4d4d"
-        p={1}
-        borderRadius="25px"
-      >
-        <Typography color="white">{waitingUser.name}</Typography>
-      </Box>
+      {waitingUser && (
+        <>
+          <Box
+            bgcolor={
+              waitingUser.gender === "M" ? theme.palette.primary.light : theme.palette.error.light
+            }
+            left="20px"
+            right="20px"
+            top="20px"
+            bottom="20px"
+            position="absolute"
+            borderRadius="50%"
+          >
+            <IconButton
+              onClick={() => onClickAvatar!(waitingUser.id)}
+              sx={{
+                position: "absolute",
+                left: "10px",
+                right: "10px",
+                top: "10px",
+                bottom: "10px",
+                width: "auto",
+                height: "auto",
+              }}
+            >
+              <Avatar
+                src={waitingUser.profile_img_path}
+                alt="profile"
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              />
+            </IconButton>
+            {waitingUser.type === 0 && (
+              <img
+                src={Crown}
+                alt="방장"
+                style={{
+                  width: "24px",
+                  height: "24px",
+                  position: "absolute",
+                  right: "0",
+                }}
+              />
+            )}
+          </Box>
+          <Box
+            position="absolute"
+            bottom="0"
+            marginBottom="10px"
+            bgcolor="#4d4d4d"
+            p={1}
+            borderRadius="25px"
+          >
+            <Typography color="white">{waitingUser.name}</Typography>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
