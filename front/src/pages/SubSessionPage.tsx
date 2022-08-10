3ab55399fda6a43c05fc6e2ee42f83fa6e5a282c
이주영 <dlendjs93@gmail.com>
2022-08-10 13:45:03 +0900

    feat: 미팅방 나가기 API 적용

--- front/src/pages/SubSessionPage.tsx
+++ front/src/pages/SubSessionPage.tsx
@@ -1,10 +1,13 @@
 import { Box, Grid } from "@mui/material";
-import React, { FC, useMemo } from "react";
+import React, { FC, useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { ControllBar } from "../components/session/ControllBar";
 import { VideoStream } from "../components/session/VideoStream";
 import { useOpenvidu } from "../hooks/useOpenvidu";
 import grey from "@mui/material/colors/grey";
+import { WS_BASE_URL } from "../apis/url";
+import SockJS from "sockjs-client";
+import * as Stomp from "stompjs";
 
 interface IProps {}
 
@@ -24,6 +27,31 @@ export const SubSessionPage: FC<IProps> = (props) => {
     () => streamList.find((it) => it.streamManager !== publisher)?.streamManager,
     [publisher, streamList]
   );
+
+  useEffect(() => {
+    if (!subRoomId || !userId) {
+      return;
+    }
+
+    const socket = new SockJS(WS_BASE_URL);
+    const client = Stomp.over(socket);
+
+    client.connect({}, function (frame) {
+      console.log("소켓 연결 성공", frame);
+    });
+
+    return () => {
+      client.send(
+        "/app/meeting/leave",
+        {},
+        JSON.stringify({
+          meetingroom_id: subRoomId,
+          user_id: userId,
+        })
+      );
+      client.disconnect(() => {});
+    };
+  }, [subRoomId, userId]);
 
   return (
     <div className="mainback">
