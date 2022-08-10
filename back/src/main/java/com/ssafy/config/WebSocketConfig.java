9cba9f82a2f881c61bd07d87170028c170e9d9ad
이주영 <dlendjs93@gmail.com>
2022-08-10 22:50:23 +0900

    refactor: WebSocket withSockJS 제거

--- back/src/main/java/com/ssafy/config/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/ava").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws/ava").setAllowedOriginPatterns("*");
 //    	registry.addEndpoint("/ws/ava");
     }
 
