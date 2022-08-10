1508b833c2a6906818d1f741575e7f57b47f8639
민경욱 <('')>
2022-08-10 19:54:47 +0900

    fix(build): stomp 버전 업그레이드

--- back/src/main/java/com/ssafy/config/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/ava").setAllowedOriginPatterns("*").withSockJS();
-//    	registry.addEndpoint("/ws/ava");
+//        registry.addEndpoint("/ws/ava").setAllowedOriginPatterns("*");
     }
 
     @Override
