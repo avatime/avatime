cce6371b0905ff7a1f35f2aabec2655b4f25b92f
정건우 <()>
2022-08-11 11:53:01 +0900

    feature(login) : 토큰 헤더 추가, 카카오 로그인 정보 미동의시 로그인 불가

--- front/src/apis/Auth.ts
+++ front/src/apis/Auth.ts
@@ -5,4 +5,5 @@ const CLIENT_ID_NAVER : string = "UZzm3q6_v7QsL_RHeEgn";
 const RAND_STATE : number = Math.floor(Math.random() * 10000)+1;
 
 export const KAKAO_AUTH_URL : string = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID_KAKAO}&redirect_uri=${REDIRECT_URI_KAKAO}&response_type=code`;
+export const KAKAO_AGREE_URL : string = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID_KAKAO}&redirect_uri=${REDIRECT_URI_KAKAO}&response_type=code&scope=account_email,gender`;
 export const NAVER_AUTH_URL : string = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID_NAVER}&redirect_uri=${REDIRECT_URI_NAVER}&state=${RAND_STATE}`;
--- front/src/apis/axiosInstance.ts
+++ front/src/apis/axiosInstance.ts
@@ -4,5 +4,8 @@ import { API_BASE_URL } from "./url";
 export let axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 1000,
-  headers: {},
+  headers: {
+    'content-type' : 'application/json',
+    'Authorization' : 'Bearer ' + localStorage.getItem("token"),
+  },
 });
