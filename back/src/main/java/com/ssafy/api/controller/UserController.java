41c2389e311f4196a4319bce8e6492472699b532
김찬영 <letgodchan0@gmail.com>
2022-08-12 15:04:46 +0900

    ﻿refactor(login): 예외처리

--- back/src/main/java/com/ssafy/api/controller/UserController.java
+++ back/src/main/java/com/ssafy/api/controller/UserController.java
@@ -22,6 +22,7 @@ import com.ssafy.api.request.UserUpdatePostReq;
 import com.ssafy.api.response.UserRes;
 import com.ssafy.api.service.ProfileService;
 import com.ssafy.api.service.UserService;
+import com.ssafy.common.auth.SsafyUserDetails;
 //import com.ssafy.common.auth.SsafyUserDetails;
 import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.Profile;
@@ -47,44 +48,7 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
-//	
-//	@PostMapping()
-//	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공"),
-//        @ApiResponse(code = 401, message = "인증 실패"),
-//        @ApiResponse(code = 404, message = "사용자 없음"),
-//        @ApiResponse(code = 500, message = "서버 오류")
-//    })
-//	public ResponseEntity<? extends BaseResponseBody> register(
-//			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
-//		
-//		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
-//		User user = userService.createUser(registerInfo);
-//		
-//		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
-//	}
-//	
-//	@GetMapping("/me")
-//	@ApiOperation(value = "회원 본인 정보 조회", notes = "로그인한 회원 본인의 정보를 응답한다.") 
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공"),
-//        @ApiResponse(code = 401, message = "인증 실패"),
-//        @ApiResponse(code = 404, message = "사용자 없음"),
-//        @ApiResponse(code = 500, message = "서버 오류")
-//    })
-//	public ResponseEntity<UserRes> getUserInfo(@ApiIgnore Authentication authentication) {
-//		/**
-//		 * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
-//		 * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
-//		 */
-//		SsafyUserDetails userDetails = (SsafyUserDetails)authentication.getDetails();
-//		String userId = userDetails.getUsername();
-//		User user = userService.getUserByUserId(userId);
-//		
-//		return ResponseEntity.status(200).body(UserRes.of(user));
-//	}
-	
+
 	// 프로필 사진 목록 조회
 	@GetMapping("/profile")
 	@ApiOperation(value = "프로필 이미지 목록 조회", notes = "서버 내 모든 프로필 이미지 제공")
@@ -107,6 +71,7 @@ public class UserController {
 	// 유저 정보 조회
 	@GetMapping("/{userId}")
 	public ResponseEntity<?> getUserInfo(@PathVariable Long userId){
+
 		User user = userService.getUserByUserId(userId);
 		if (user != null) {
 			return ResponseEntity.status(201).body(user);
