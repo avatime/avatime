9892bc8aeacfed9b41e563a15c19bfe7609444e3
이지나 <dlwlsk0428@naver.com>
2022-08-12 16:09:06 +0900

    feat(mask): 아바타 업로드

--- back/src/main/java/com/ssafy/api/dto/FileDetail.java
+++ back/src/main/java/com/ssafy/api/dto/FileDetail.java
@@ -31,13 +31,12 @@ public class FileDetail {
     @Builder.Default
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    public static FileDetail multipartOf(AvatarCustomReq avatarCustomReq) {
-    	MultipartFile multipartFile = avatarCustomReq.getImage();
-        final String fileId = MultipartUtil.createFileId();
+    public static FileDetail multipartOf(MultipartFile multipartFile) {
+    	final String fileId = MultipartUtil.createFileId();
         final String format = MultipartUtil.getFormat(multipartFile.getContentType());
         return FileDetail.builder()
                 .id(fileId)
-                .name(avatarCustomReq.getAvatar_name())
+                .name(multipartFile.getOriginalFilename())
                 .format(format)
                 .path(MultipartUtil.createPath(fileId, format))
                 .bytes(multipartFile.getSize())
