43910ef995d194cc21ad4998e75052fb1995e25e
민경욱 <('')>
2022-08-12 17:34:25 +0900

    feat(balance): 밸런스게임 entity 생성

---
+++ back/src/main/java/com/ssafy/db/entity/Balance.java
@@ -0,0 +1,26 @@
+package com.ssafy.db.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+
+
+@Entity
+@Getter
+public class Balance {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private int id;
+	
+	@Column(updatable = false, nullable = false)
+	private String a;
+	
+	@Column(updatable = false, nullable = false)
+	private String b;
+}
