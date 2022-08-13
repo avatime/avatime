2600b5cb5fd2bf805045727b19f1d35ce1f02427
채윤선 <('')>
2022-08-13 22:40:29 +0900

    style(cusor) : 커서변경

--- front/src/pages/Landing/LandingPage.tsx
+++ front/src/pages/Landing/LandingPage.tsx
@@ -1,13 +1,9 @@
-import { Box } from "@mui/material";
-import React, { FC, useEffect } from "react";
+import { Box, Button } from "@mui/material";
+import React, { FC } from "react";
 import "./LandingPage.css";
 import { Link } from "react-router-dom";
 import "animate.css";
 import "aos/dist/aos.css";
-import { useDispatch } from 'react-redux';
-import { reset } from "../../stores/slices/userSlice";
-import { resetWaiting } from "../../stores/slices/waitingSlice";
-import { resetMeeting } from "../../stores/slices/meetingSlice";
 
 interface IProps {}
 
@@ -17,46 +13,39 @@ interface IProps {}
  **/
 
 export const LandingPage: FC<IProps> = (props) => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    localStorage.clear();
-    dispatch(reset());
-    dispatch(resetWaiting());
-    dispatch(resetMeeting());
-  }, [dispatch]);
-  
   return (
     <div>
-      <Box className="landing">
+      <Box className="landing" sx={{flexDirection:"column"}}>
+        <h1 className="landing--h1">
+          <span className="landing--h1--span">A</span>
+          <span className="landing--h1--span">V</span>
+          <span className="landing--h1--span">A</span>
+          <span className="landing--h1--span">T</span>
+          <span className="landing--h1--span">I</span>
+          <span className="landing--h1--span">M</span>
+          <span className="landing--h1--span">E</span>
+        </h1>
+        <br />
+        <h2 className="landing--h2">
+          <span className="landing--h2--span">내</span>
+          <span className="landing--h2--span">가</span>
+          <span className="landing--h2--span"> </span>
+          <span className="landing--h2--span">만</span>
+          <span className="landing--h2--span">든</span>
+          <span className="landing--h2--span"> </span>
+          <span className="landing--h2---span">가</span>
+          <span className="landing--h2---span">면</span>
+          <span className="landing--h2--span">으</span>
+          <span className="landing--h2--span">로</span>
+          <span className="landing--h2--span"> </span>
+          <span className="landing--h2---span">미</span>
+          <span className="landing--h2---span">팅</span>
+          <span className="landing--h2--span">하</span>
+          <span className="landing--h2--span">자</span>
+        </h2>
+        <Box p={3} />
         <Link to="/login">
-          <h1 className="landing--h1">
-            <span className="landing--h1--span">A</span>
-            <span className="landing--h1--span">V</span>
-            <span className="landing--h1--span">A</span>
-            <span className="landing--h1--span">T</span>
-            <span className="landing--h1--span">I</span>
-            <span className="landing--h1--span">M</span>
-            <span className="landing--h1--span">E</span>
-          </h1>
-          <br />
-          <Box p={3} />
-          <h2 className="landing--h2">
-            <span className="landing--h2--span">내</span>
-            <span className="landing--h2--span">가</span>
-            <span className="landing--h2--span">  </span>
-            <span className="landing--h2--span">만</span>
-            <span className="landing--h2--span">든</span>
-            <span className="landing--h2--span">  </span>
-            <span className="landing--h2---span">가</span>
-            <span className="landing--h2---span">면</span>
-            <span className="landing--h2--span">으</span>
-            <span className="landing--h2--span">로</span>
-            <span className="landing--h2--span">  </span>
-            <span className="landing--h2---span">미</span>
-            <span className="landing--h2---span">팅</span>
-            <span className="landing--h2--span">하</span>
-            <span className="landing--h2--span">자</span>
-          </h2>
+          <Button variant="outlined">start</Button>
         </Link>
       </Box>
     </div>
