7fe6e43a83214798f98c2dd14ea10b1101cdab1f
이주영 <()>
2022-08-13 19:45:44 +0900

    refactor: 토큰 예외 api 추가

--- back/src/main/java/com/ssafy/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -24,50 +24,46 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private SsafyUserDetailService ssafyUserDetailService;
-    
-    @Autowired
-    private UserService userService;
-    
-    // Password 인코딩 방식에 BCrypt 암호화 방식 사용
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Autowired
+	private SsafyUserDetailService ssafyUserDetailService;
 
-    // DAO 기반으로 Authentication Provider를 생성
-    // BCrypt Password Encoder와 UserDetailService 구현체를 설정
-    @Bean
-    DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(this.ssafyUserDetailService);
-        return daoAuthenticationProvider;
-    }
+	@Autowired
+	private UserService userService;
 
-    // DAO 기반의 Authentication Provider가 적용되도록 설정
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider());
-    }
+	// Password 인코딩 방식에 BCrypt 암호화 방식 사용
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        	.httpBasic().disable()
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
-            .and()
-            .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
-            .authorizeRequests()
-            .antMatchers("/api/v1/**","/api/v1/auth/register", "/api/v1/auth/naver", "/api/v1/auth/kakao", "/v2/api-docs/**", "/swagger*/**").permitAll()
-            .anyRequest().authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
-            .and().cors();
-        http
-        	.headers()
-        	.xssProtection()
-        	.and()
-        	.contentSecurityPolicy("script-src 'self'");
-    }
+	// DAO 기반으로 Authentication Provider를 생성
+	// BCrypt Password Encoder와 UserDetailService 구현체를 설정
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		daoAuthenticationProvider.setUserDetailsService(this.ssafyUserDetailService);
+		return daoAuthenticationProvider;
+	}
+
+	// DAO 기반의 Authentication Provider가 적용되도록 설정
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().disable().csrf().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
+				.and().addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) // HTTP 요청에 JWT 토큰
+																									// 인증 필터를 거치도록 필터를
+																									// 추가
+				.authorizeRequests()
+				.antMatchers("/api/v1/user/check/*", "/api/v1/user/profile", "/api/v1/auth/register",
+						"/api/v1/auth/naver", "/api/v1/auth/kakao", "/v2/api-docs/**", "/swagger*/**", "/ws/ava/**")
+				.permitAll().anyRequest().authenticated() // 인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
+				.and().cors();
+		http.headers().xssProtection().and().contentSecurityPolicy("script-src 'self'");
+	}
 }
