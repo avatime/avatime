526dc286df1e9b82e6824aa572eb53f7b6d663a6
민경욱 <('')>
2022-08-15 15:45:30 +0900

    fix(entity): entity @setter 삭제

--- back/src/main/java/com/ssafy/db/entity/Balance.java
+++ back/src/main/java/com/ssafy/db/entity/Balance.java
@@ -9,13 +9,11 @@ import org.hibernate.annotations.DynamicUpdate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 
 @DynamicInsert @DynamicUpdate
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Balance extends BaseEntity {
