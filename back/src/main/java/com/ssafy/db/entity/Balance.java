9aee5d2526b5fdf3f299aad0003fc94d9bc60875
민경욱 <('')>
2022-08-15 14:15:49 +0900

    feat(balance): entity 완료

--- back/src/main/java/com/ssafy/db/entity/Balance.java
+++ back/src/main/java/com/ssafy/db/entity/Balance.java
@@ -2,21 +2,13 @@ package com.ssafy.db.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import lombok.Getter;
 
 
 @Entity
 @Getter
-public class Balance {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(updatable = false, nullable = false)
-	private int id;
+public class Balance extends BaseEntity{
 	
 	@Column(updatable = false, nullable = false)
 	private String a;
